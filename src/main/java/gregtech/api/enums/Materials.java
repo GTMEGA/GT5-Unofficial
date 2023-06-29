958f21d061b32ae2a097630a99eb949562f346e7
Houston <Houston.russ@hotmail.com>
2023-06-29 02:42:11 -0400

    Removed gem EBF smelting for aluminum Brassin' Up RC machines. Disables kanthal as vacuum freezer ingot

--- src/main/java/gregtech/api/enums/Materials.java
+++ src/main/java/gregtech/api/enums/Materials.java
@@ -2194,6 +2194,7 @@ public class Materials implements IColorModulationContainer, ISubTagContainer {
             OrePrefixes.ingotHot.disableComponent(Materials.EnergeticAlloy);
             OrePrefixes.ingotHot.disableComponent(Materials.PulsatingIron);
             OrePrefixes.ingotHot.disableComponent(Materials.CrudeSteel);
+            OrePrefixes.ingotHot.disableComponent(Materials.Kanthal);
         }
 
         fillGeneratedMaterialsMap();
