16197717a4f5fd4b3de3826fab103fbac4f75495
Charles Johnson <johnch18@isu.edu>
2023-08-30 23:06:45 -0600

    Added basic stuff for pre-calcing explosions

--- src/main/java/gregtech/common/misc/explosions/GT_Explosion_PreCalculation.java
+++ src/main/java/gregtech/common/misc/explosions/GT_Explosion_PreCalculation.java
@@ -7,12 +7,16 @@ import net.minecraft.world.World;
 
 @Getter
 @RequiredArgsConstructor
-public class GT_Explosion_PreCalculation {
+public abstract class GT_Explosion_PreCalculation {
 
     private final GT_Entity_Explosive explosionSource;
 
     private final World world;
 
     private final int sourceX, sourceY, sourceZ;
+
+    public void tick() {
+
+    }
 
 }
