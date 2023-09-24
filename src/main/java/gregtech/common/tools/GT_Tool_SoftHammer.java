f453c8be06c053fc4b3584781f3c93f823e37ff0
Houston <houston.russ@hotmail.com>
2023-09-24 05:04:38 -0400

    Brass tool level 1 -> 3 Bronze tool speed + .5 Black Bronze fortune 2 Rubber Durability x2 Soft mallet craft damage x0.5

--- src/main/java/gregtech/common/tools/GT_Tool_SoftHammer.java
+++ src/main/java/gregtech/common/tools/GT_Tool_SoftHammer.java
@@ -26,7 +26,7 @@ public class GT_Tool_SoftHammer extends GT_Tool {
 
     @Override
     public int getToolDamagePerContainerCraft() {
-        return 800;
+        return 400;
     }
 
     @Override
