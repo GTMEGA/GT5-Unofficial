5700ccacfd9603a51866d643dbf31737b8f4f6ca
Houston <houston.russ@hotmail.com>
2023-10-10 22:49:29 -0400

    woe be opun thee; for lignite is gone - removed lignite from oregen.

--- src/main/java/gregtech/loaders/postload/GT_Worldgenloader.java
+++ src/main/java/gregtech/loaders/postload/GT_Worldgenloader.java
@@ -128,8 +128,8 @@ public class GT_Worldgenloader implements Runnable {
             //GT Default Veins
 
             new GT_Worldgen_GT_Ore_Layer("ore.mix.naquadah", true, 10, 90, 30, 4, 32, false, false, false, Materials.Naquadah, Materials.Naquadah, Materials.Naquadah, Materials.NaquadahEnriched);
-            new GT_Worldgen_GT_Ore_Layer("ore.mix.lignite", true, 80, 210, 20, 7, 32, true, false, false, Materials.Lignite, Materials.Lignite, Materials.Lignite, Materials.Coal);
-            new GT_Worldgen_GT_Ore_Layer("ore.mix.coal", true, 50, 80, 140, 5, 32, true, false, false, Materials.Coal, Materials.Coal, Materials.Coal, Materials.Lignite);
+            new GT_Worldgen_GT_Ore_Layer("ore.mix.lignite", false, 80, 210, 20, 7, 32, false, false, false, Materials.Lignite, Materials.Lignite, Materials.Lignite, Materials.Coal);
+            new GT_Worldgen_GT_Ore_Layer("ore.mix.coal", true, 50, 80, 140, 5, 32, true, false, false, Materials.Coal, Materials.Coal, Materials.Coal, Materials.Coal);
             new GT_Worldgen_GT_Ore_Layer("ore.mix.magnetite", true, 55, 180, 120, 2, 32, true, false, false, Materials.Magnetite, Materials.Magnetite, Materials.VanadiumMagnetite, Materials.Iron);
             new GT_Worldgen_GT_Ore_Layer("ore.mix.gold", true, 15, 45, 100, 2, 24, true, false, true, Materials.Magnetite, Materials.Magnetite, Materials.Gold, Materials.VanadiumMagnetite);
             new GT_Worldgen_GT_Ore_Layer("ore.mix.iron", true, 35, 55, 90, 3, 24, true, false, false, Materials.BrownLimonite, Materials.YellowLimonite, Materials.BandedIron, Materials.Malachite);
