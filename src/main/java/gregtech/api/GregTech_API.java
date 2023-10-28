6c1936951a626bcbc751833ee1672a1f0ee46fc9
Houston <houston.russ@hotmail.com>
2023-10-28 12:15:40 -0400

    funni macerator sound

--- src/main/java/gregtech/api/GregTech_API.java
+++ src/main/java/gregtech/api/GregTech_API.java
@@ -394,6 +394,8 @@ public class GregTech_API {
         sSoundList.put(219, MOD_ID.toLowerCase() + ":" + "remote_detonator_remove");
         sSoundList.put(220, MOD_ID.toLowerCase() + ":" + "remote_detonator_trigger");
         sSoundList.put(221, MOD_ID.toLowerCase() + ":" + "potentiometer_click");
+        sSoundList.put(222, MOD_ID.toLowerCase() + ":" + "macerator");
+        //Don't forget to put your sounds into sounds.json too!
     }
 
     /**
