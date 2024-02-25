e3d4bd792189a400861ce4939c5e2d5824681bd1
matt-159 <16054364+matt-159@users.noreply.github.com>
2024-02-25 13:58:20 -0800

    no longer a solid waste of an item...

--- src/main/java/gregtech/common/items/GT_SolidWaste.java
+++ src/main/java/gregtech/common/items/GT_SolidWaste.java
@@ -1,6 +1,14 @@
 package gregtech.common.items;
 
+import gregtech.api.events.GT_OreVeinLocations;
 import gregtech.api.items.GT_Generic_Item;
+import lombok.val;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.ChunkCoordIntPair;
+import net.minecraft.world.World;
 
 public class GT_SolidWaste extends GT_Generic_Item {
     public GT_SolidWaste() {
@@ -8,5 +16,25 @@ public class GT_SolidWaste extends GT_Generic_Item {
               "Solid Waste",
               "Solid Waste Description",
               true);
+    }
+
+    @Override
+    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world,
+                             int x, int y, int z, int side,
+                             float hitX, float hitY, float hitZ) {
+        if (world.isRemote) {
+            return false;
+        }
+
+        val chunkCoordinate = new ChunkCoordIntPair(x >> 4, z >> 4);
+        val oreMix = GT_OreVeinLocations.RecordedOreVeinInChunk.get(world.provider.dimensionId,
+                                                                    chunkCoordinate);
+
+        player.addChatMessage(new ChatComponentText(String.format("[%s, %s] -> %s",
+                                                                  chunkCoordinate.chunkXPos,
+                                                                  chunkCoordinate.chunkZPos,
+                                                                  oreMix)));
+
+        return false;
     }
 }
