30b86032b1c7bb7315ef5b42f7d6d89d49ec43e2
Ven <tudurap.com@gmail.com>
2024-05-04 05:53:00 +0100

    BALLER BALLER BALLER!!!

--- src/main/java/gregtech/api/events/DrillDrawBlockHighlightEvent.java
+++ src/main/java/gregtech/api/events/DrillDrawBlockHighlightEvent.java
@@ -1,279 +1,274 @@
 package gregtech.api.events;
 
-import com.falsepattern.falsetweaks.Compat;
-import gregtech.api.interfaces.IToolStats;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import gregtech.api.items.GT_MetaGenerated_Tool;
 import gregtech.common.items.GT_MetaGenerated_Tool_01;
 import gregtech.common.tools.GT_Tool_Drill_LV;
 import lombok.val;
+import lombok.var;
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import org.lwjgl.opengl.GL11;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.EntityClientPlayerMP;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.MovingObjectPosition;
-import net.minecraft.world.ChunkPosition;
-import net.minecraft.world.World;
-import net.minecraftforge.client.event.DrawBlockHighlightEvent;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-
-import java.awt.Color;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
 
 @SideOnly(Side.CLIENT)
 public class DrillDrawBlockHighlightEvent {
+    private static final float LINE_WIDTH = 5F; // Same as MEGA default
+    private static final float LINE_OFFSET = 0.002F; // Same as MEGA default
+
+    private static final Color LINE_COLOR = new Color(26, 51, 230, 153); // Just here for the color picker
+    private static final float LINE_COLOR_R = LINE_COLOR.getRed() / 255F;
+    private static final float LINE_COLOR_G = LINE_COLOR.getGreen() / 255F;
+    private static final float LINE_COLOR_B = LINE_COLOR.getBlue() / 255F;
+    private static final float LINE_COLOR_A = LINE_COLOR.getAlpha() / 255F;
+
     @SubscribeEvent
     public void onBlockDrawHighlight(DrawBlockHighlightEvent event) {
-        if (event.currentItem == null) {
-            return;
-        }
-
-        val heldItem = event.currentItem.getItem();
-
-        if (!(heldItem instanceof GT_MetaGenerated_Tool_01)) {
-            return;
-        }
-
-        val heldTool = (GT_MetaGenerated_Tool_01) heldItem;
-        val heldToolType = heldTool.getToolStats(event.currentItem);
-
-        if (!(heldToolType instanceof GT_Tool_Drill_LV)) {
-            return;
-        }
-
-        val aoe = ((GT_Tool_Drill_LV) heldToolType).getAOE(GT_MetaGenerated_Tool.getStatNbt(event.currentItem));
-
-        if (event.target.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK) {
-            return;
-        }
-
-        if (event.player == null) {
-            return;
-        }
-
-        val list = this.getBlocksToBreak(event.target.sideHit,
-                                         GT_Tool_Drill_LV.xStart[aoe], GT_Tool_Drill_LV.yStart[aoe],
-                                         GT_Tool_Drill_LV.xLen[aoe], GT_Tool_Drill_LV.yLen[aoe],
-                                         event.target.blockX, event.target.blockY, event.target.blockZ, event.player);
-
-        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
-        GL11.glPushMatrix();
-
         val player = event.player;
-        val partialTick = event.partialTicks;
+        if (player == null)
+            return;
 
-        val offsetX = (float) (player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTick);
-        val offsetY = (float) (player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTick);
-        val offsetZ = (float) (player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTick);
+        val renderGlobal = event.context;
+        if (renderGlobal == null)
+            return;
 
-        GL11.glTranslatef(-offsetX, -offsetY, -offsetZ);
-        GL11.glLineWidth(5F);
-        GL11.glColor4f(0.1F, 0.2F, 0.9F, 0.6F);
+        val rayHit = event.target;
+        if (rayHit == null || rayHit.typeOfHit != MovingObjectPosition.MovingObjectType.BLOCK)
+            return;
 
-//        val color = Color.CYAN;
-        val tessellator = Tessellator.instance;
+        val posX = rayHit.blockX;
+        val posY = rayHit.blockY;
+        val posZ = rayHit.blockZ;
+        val side = rayHit.sideHit;
 
-        tessellator.setColorRGBA(26,32,230,153);
-        tessellator.startDrawing(GL11.GL_LINES);
+        val itemStack = event.currentItem;
+        if (itemStack == null)
+            return;
+        val item = itemStack.getItem();
+        if (item == null)
+            return;
 
-        for (val position : list) {
-            val minX = position.chunkPosX - 0.001F;
-            val minY = position.chunkPosY - 0.001F;
-            val minZ = position.chunkPosZ - 0.001F;
+        if (!(item instanceof GT_MetaGenerated_Tool_01))
+            return;
+        val tool = (GT_MetaGenerated_Tool_01) item;
+        val toolType = tool.getToolStats(event.currentItem);
+        if (!(toolType instanceof GT_Tool_Drill_LV))
+            return;
 
-            val maxX = minX + 1 + 0.001F;
-            val maxY = minY + 1 + 0.008F;
-            val maxZ = minZ + 1 + 0.001F;
+        val drillType = (GT_Tool_Drill_LV) toolType;
+        val nbtTag = GT_MetaGenerated_Tool.getStatNbt(itemStack);
+        if (nbtTag == null)
+            return;
+        val aoe = drillType.getAOE(nbtTag);
 
-            tessellator.addVertex(maxX, minY, maxZ); tessellator.addVertex(maxX, maxY, maxZ);
-            tessellator.addVertex(maxX, minY, minZ); tessellator.addVertex(maxX, minY, maxZ);
-            tessellator.addVertex(maxX, minY, minZ); tessellator.addVertex(maxX, maxY, minZ);
-            tessellator.addVertex(maxX, maxY, minZ); tessellator.addVertex(maxX, maxY, maxZ);
+        val aabbList = getBlockBoundsInRange(side,
+                                             GT_Tool_Drill_LV.xStart[aoe],
+                                             GT_Tool_Drill_LV.yStart[aoe],
+                                             GT_Tool_Drill_LV.xLen[aoe],
+                                             GT_Tool_Drill_LV.yLen[aoe],
+                                             posX,
+                                             posY,
+                                             posZ,
+                                             player);
 
-            tessellator.addVertex(minX, minY, minZ); tessellator.addVertex(minX, maxY, minZ);
-            tessellator.addVertex(minX, minY, maxZ); tessellator.addVertex(minX, maxY, maxZ);
-            tessellator.addVertex(minX, minY, minZ); tessellator.addVertex(minX, minY, maxZ);
-            tessellator.addVertex(minX, maxY, minZ); tessellator.addVertex(minX, maxY, maxZ);
+        GL11.glPushMatrix();
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        {
+            GL11.glLineWidth(LINE_WIDTH);
+            GL11.glEnable(GL11.GL_LINE_SMOOTH);
+            // Basically this will look goofy-ah in dev, but it will be correct with MegaBlox 1.5.0+
+            GL11.glColor4f(LINE_COLOR_R, LINE_COLOR_G, LINE_COLOR_B, LINE_COLOR_A);
 
-            tessellator.addVertex(minX, maxY, minZ); tessellator.addVertex(maxX, maxY, minZ);
-            tessellator.addVertex(minX, maxY, minZ); tessellator.addVertex(minX, maxY, maxZ);
-            tessellator.addVertex(minX, maxY, maxZ); tessellator.addVertex(maxX, maxY, maxZ);
-            tessellator.addVertex(maxX, maxY, minZ); tessellator.addVertex(maxX, maxY, maxZ);
+            val partialTick = event.partialTicks;
+            val offsetX = (float) (player.lastTickPosX + (player.posX - player.lastTickPosX) * partialTick);
+            val offsetY = (float) (player.lastTickPosY + (player.posY - player.lastTickPosY) * partialTick);
+            val offsetZ = (float) (player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * partialTick);
+            GL11.glTranslatef(-offsetX, -offsetY, -offsetZ);
 
-            tessellator.addVertex(minX, minY, minZ); tessellator.addVertex(maxX, minY, minZ);
-            tessellator.addVertex(minX, minY, minZ); tessellator.addVertex(minX, minY, maxZ);
-            tessellator.addVertex(minX, minY, maxZ); tessellator.addVertex(maxX, minY, maxZ);
-            tessellator.addVertex(maxX, minY, minZ); tessellator.addVertex(maxX, minY, maxZ);
-
-            tessellator.addVertex(minX, minY, maxZ); tessellator.addVertex(maxX, minY, maxZ);
-            tessellator.addVertex(minX, maxY, maxZ); tessellator.addVertex(maxX, maxY, maxZ);
-            tessellator.addVertex(minX, minY, maxZ); tessellator.addVertex(minX, maxY, maxZ);
-            tessellator.addVertex(maxX, minY, maxZ); tessellator.addVertex(maxX, maxY, maxZ);
-
-            tessellator.addVertex(minX, minY, minZ); tessellator.addVertex(maxX, minY, minZ);
-            tessellator.addVertex(minX, maxY, minZ); tessellator.addVertex(maxX, maxY, minZ);
-            tessellator.addVertex(minX, minY, minZ); tessellator.addVertex(minX, maxY, minZ);
-            tessellator.addVertex(maxX, minY, minZ); tessellator.addVertex(maxX, maxY, minZ);
+            aabbList.forEach(aabb -> RenderGlobal.drawOutlinedBoundingBox(aabb, -1));
         }
-
-        tessellator.draw();
-
-        GL11.glPopMatrix();
         GL11.glPopAttrib();
+        GL11.glPopMatrix();
     }
 
     // copy pasted from IAOETool.breakBlockAround
-    private List<ChunkPosition> getBlocksToBreak(int side, int xStartPos, int yStartPos, int xLength, int yLength, int x, int y, int z, EntityPlayer player) {
-        val list = new ArrayList<ChunkPosition>();
+    //
+    // edited to be cooling
+    //
+    private static List<AxisAlignedBB> getBlockBoundsInRange(int side,
+                                                             int negWidth,
+                                                             int negHeight,
+                                                             int posWidth,
+                                                             int posHeight,
+                                                             int centerX,
+                                                             int centerY,
+                                                             int centerZ,
+                                                             EntityPlayer player) {
+        val world = player.worldObj;
+        if (world == null)
+            return Collections.emptyList();
 
-        int DX;
-        int DY;
-        int DZ;
-        boolean downUp = side < 2;
-        boolean northSouth = side < 4;
-        float rotation = player.rotationYawHead < 0 ? 360f + player.rotationYawHead : player.rotationYawHead;
+        val aabbList = new ArrayList<AxisAlignedBB>();
+
+        val downUp = side < 2;
+        val northSouth = side < 4;
+        val rotation = player.rotationYawHead < 0 ? 360F + player.rotationYawHead : player.rotationYawHead;
 
         BiFunction<Integer, Integer, Boolean> xCheck = (value, len) -> value < len;
         BiFunction<Integer, Integer, Boolean> yCheck = (value, len) -> value > len;
 
-        int xIterate = 1;
-        int yIterate = -1;
+        var xIterate = 1;
+        var yIterate = -1;
 
         if (downUp) {
-            boolean north = rotation > 135 && rotation < 225;// 180
+            val north = rotation > 135 && rotation < 225;// 180
             //boolean east = rotation >= 225 && rotation <= 315;//270
-            boolean west = rotation >= 45 && rotation <= 135;// 90
-            boolean south = rotation < 45 || rotation > 315;//0
+            val west = rotation >= 45 && rotation <= 135;// 90
+            val south = rotation < 45 || rotation > 315;//0
 
             //x = north-south
             //y = east-west
 
             if (south) {
-                int tempX = xLength;
-                xLength = yLength;
-                yLength = tempX;
+                int tempX = posWidth;
+                posWidth = posHeight;
+                posHeight = tempX;
                 //invert Y direction
-                yLength *= -1;
-                yLength -= yStartPos;
-                yStartPos *= -1;
+                posHeight *= -1;
+                posHeight -= negHeight;
+                negHeight *= -1;
                 //invert X direction when looking down
                 if (side == 0) {
-                    xLength += xStartPos;
+                    posWidth += negWidth;
                 } else {
                     xCheck = (value, len) -> value > len;
-                    xLength *= -1;
-                    xLength -= xStartPos;
+                    posWidth *= -1;
+                    posWidth -= negWidth;
                     xIterate = -1;
-                    xStartPos *= -1;
+                    negWidth *= -1;
                 }
             } else if (west) {
                 //invert X direction
                 xCheck = (value, len) -> value > len;
-                xLength *= -1;
-                xLength -= xStartPos;
+                posWidth *= -1;
+                posWidth -= negWidth;
                 xIterate = -1;
-                xStartPos *= -1;
+                negWidth *= -1;
                 //invert Y direction when looking up
                 if (side == 0) {
-                    yLength *= -1;
-                    yLength -= yStartPos;
-                    yStartPos *= -1;
+                    posHeight *= -1;
+                    posHeight -= negHeight;
+                    negHeight *= -1;
                 } else {
                     yCheck = (value, len) -> value < len;
                     yIterate = 1;
-                    yLength += yStartPos;
+                    posHeight += negHeight;
                 }
             } else if (north) {
-                int tempX = xLength;
-                xLength = yLength;
-                yLength = tempX;
+                int tempX = posWidth;
+                posWidth = posHeight;
+                posHeight = tempX;
                 //dont invert Y direction
                 yCheck = (value, len) -> value < len;
                 yIterate = 1;
-                yLength += yStartPos;
+                posHeight += negHeight;
 
                 //invert X direction when looking UP
                 if (side == 0) {
                     xCheck = (value, len) -> value > len;
-                    xLength *= -1;
-                    xLength -= xStartPos;
+                    posWidth *= -1;
+                    posWidth -= negWidth;
                     xIterate = -1;
-                    xStartPos *= -1;
+                    negWidth *= -1;
                 } else {
-                    xLength += xStartPos;
+                    posWidth += negWidth;
                 }
             } else {
                 //dont invert X direction
-                xLength += xStartPos;
+                posWidth += negWidth;
 
                 //invert Y direction when looking DOWN
                 if (side == 0) {
                     yCheck = (value, len) -> value < len;
                     yIterate = 1;
-                    yLength += yStartPos;
+                    posHeight += negHeight;
                 } else {
-                    yLength *= -1;
-                    yLength -= yStartPos;
-                    yStartPos *= -1;
+                    posHeight *= -1;
+                    posHeight -= negHeight;
+                    negHeight *= -1;
                 }
             }
         } else {
-            yLength *= -1;
-            yLength -= yStartPos;
-            yStartPos *= -1;
+            posHeight *= -1;
+            posHeight -= negHeight;
+            negHeight *= -1;
             if (northSouth) {
                 if (side == 2) {
-                    xLength *= -1;
-                    xLength -= xStartPos;
+                    posWidth *= -1;
+                    posWidth -= negWidth;
                     xIterate = -1;
-                    xStartPos *= -1;
+                    negWidth *= -1;
                     xCheck = (value, len) -> value > len;
                 } else {
-                    xLength += xStartPos;
+                    posWidth += negWidth;
                 }
             } else {
                 if (side == 5) {
-                    xLength *= -1;
-                    xLength -= xStartPos;
+                    posWidth *= -1;
+                    posWidth -= negWidth;
                     xIterate = -1;
-                    xStartPos *= -1;
+                    negWidth *= -1;
                     xCheck = (value, len) -> value > len;
                 } else {
-                    xLength += xStartPos;
+                    posWidth += negWidth;
                 }
             }
         }
 
-        for (int Y = yStartPos; yCheck.apply(Y, yLength); Y += yIterate) {
-            for (int X = xStartPos; xCheck.apply(X, xLength); X += xIterate) {
-                if (Y != 0 || X != 0) {
-                    if (downUp) {
-                        //im to lazy to fix this
-                        DX = x + Y;
-                        DY = y;
-                        DZ = z + X;
-                    } else if (northSouth) {
-                        DX = x + X;
-                        DY = y + Y;
-                        DZ = z;
-                    } else {
-                        DX = x;
-                        DY = y + Y;
-                        DZ = z + X;
-                    }
+        for (var offsetY = negHeight; yCheck.apply(offsetY, posHeight); offsetY += yIterate) {
+            for (var offsetX = negWidth; xCheck.apply(offsetX, posWidth); offsetX += xIterate) {
+                if (offsetY == 0 && offsetX == 0)
+                    continue;
 
-                    list.add(new ChunkPosition(DX, DY, DZ));
+                final int posX;
+                final int posY;
+                final int posZ;
+                if (downUp) {
+                    //im to lazy to fix this
+                    posX = centerX + offsetY;
+                    posY = centerY;
+                    posZ = centerZ + offsetX;
+                } else if (northSouth) {
+                    posX = centerX + offsetX;
+                    posY = centerY + offsetY;
+                    posZ = centerZ;
+                } else {
+                    posX = centerX;
+                    posY = centerY + offsetY;
+                    posZ = centerZ + offsetX;
                 }
+
+                val block = world.getBlock(posX, posY, posZ);
+                if (block == null || block.isAir(world, posX, posY, posZ))
+                    continue;
+
+                block.setBlockBoundsBasedOnState(world, posX, posY, posZ);
+                val aabb = block.getSelectedBoundingBoxFromPool(world, posX, posY, posZ)
+                                .expand(LINE_OFFSET, LINE_OFFSET, LINE_OFFSET);
+                aabbList.add(aabb);
             }
         }
 
-        return list;
+        return aabbList;
     }
 }
