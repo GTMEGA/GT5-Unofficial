559e2d7f723fe24e802adbc4def858c6e7df91a8
FalsePattern <me@falsepattern.com>
2024-12-31 19:08:24 +0100

    fix Z fighting on block bottom face

--- src/main/java/gregtech/api/objects/GT_RenderedTexture.java
+++ src/main/java/gregtech/api/objects/GT_RenderedTexture.java
@@ -78,97 +78,16 @@ public class GT_RenderedTexture implements ITexture, IColorModulationContainer {
         }
     }
 
+
     @Override
     public void renderYNeg(RenderBlocks aRenderer, Block aBlock, int aX, int aY, int aZ) {
-        final Tessellator tessellator = GT_Compat.tessellator();
-        IIcon aIcon = mIconContainer.getIcon();
-
-        float minU = aIcon.getInterpolatedU((1.0D - aRenderer.renderMaxX) * 16.0D);
-        float maxU = aIcon.getInterpolatedU((1.0D - aRenderer.renderMinX) * 16.0D);
-        float minV = aIcon.getInterpolatedV(aRenderer.renderMinZ * 16.0D);
-        float maxV = aIcon.getInterpolatedV(aRenderer.renderMaxZ * 16.0D);
-
-        if (aRenderer.renderMinX < 0.0D || aRenderer.renderMaxX > 1.0D) {
-            minU = 16.0F - aIcon.getMaxU();
-            maxU = 16.0F - aIcon.getMinU();
-        }
-
-        if (aRenderer.renderMinZ < 0.0D || aRenderer.renderMaxZ > 1.0D) {
-            minV = aIcon.getMinV();
-            maxV = aIcon.getMaxV();
-        }
-
-        double minX = aX + aRenderer.renderMinX;
-        double maxX = aX + aRenderer.renderMaxX;
-        double minY = aY + aRenderer.renderMinY;
-        double minZ = aZ + aRenderer.renderMinZ;
-        double maxZ = aZ + aRenderer.renderMaxZ;
-
         LightingHelper lighting = new LightingHelper(aRenderer);
         lighting.setupLightingYNeg(aBlock, aX, aY, aZ)
                 .setupColor(ForgeDirection.DOWN.ordinal(), mRGBa);
-
-        if (aRenderer.enableAO) {
-            tessellator.setColorOpaque_F(aRenderer.colorRedTopLeft, aRenderer.colorGreenTopLeft, aRenderer.colorBlueTopLeft);
-            tessellator.setBrightness(aRenderer.brightnessTopLeft);
-            tessellator.addVertexWithUV(minX, minY, maxZ, maxU, maxV);
-            tessellator.setColorOpaque_F(aRenderer.colorRedBottomLeft, aRenderer.colorGreenBottomLeft, aRenderer.colorBlueBottomLeft);
-            tessellator.setBrightness(aRenderer.brightnessBottomLeft);
-            tessellator.addVertexWithUV(minX, minY, minZ, maxU, minV);
-            tessellator.setColorOpaque_F(aRenderer.colorRedBottomRight, aRenderer.colorGreenBottomRight, aRenderer.colorBlueBottomRight);
-            tessellator.setBrightness(aRenderer.brightnessBottomRight);
-            tessellator.addVertexWithUV(maxX, minY, minZ, minU, minV);
-            tessellator.setColorOpaque_F(aRenderer.colorRedTopRight, aRenderer.colorGreenTopRight, aRenderer.colorBlueTopRight);
-            tessellator.setBrightness(aRenderer.brightnessTopRight);
-        } else {
-            tessellator.addVertexWithUV(minX, minY, maxZ, maxU, maxV);
-            tessellator.addVertexWithUV(minX, minY, minZ, maxU, minV);
-            tessellator.addVertexWithUV(maxX, minY, minZ, minU, minV);
-        }
-        tessellator.addVertexWithUV(maxX, minY, maxZ, minU, maxV);
-
+        aRenderer.renderFaceYNeg(aBlock, aX, aY, aZ, mIconContainer.getIcon());
         if (mIconContainer.getOverlayIcon() != null) {
-            minU = aIcon.getInterpolatedU((1.0D - aRenderer.renderMaxX) * 16.0D);
-            maxU = aIcon.getInterpolatedU((1.0D - aRenderer.renderMinX) * 16.0D);
-            minV = aIcon.getInterpolatedV(aRenderer.renderMinZ * 16.0D);
-            maxV = aIcon.getInterpolatedV(aRenderer.renderMaxZ * 16.0D);
-
-            if (aRenderer.renderMinX < 0.0D || aRenderer.renderMaxX > 1.0D) {
-                minU = 16.0F - aIcon.getMaxU();
-                maxU = 16.0F - aIcon.getMinU();
-            }
-
-            if (aRenderer.renderMinZ < 0.0D || aRenderer.renderMaxZ > 1.0D) {
-                minV = aIcon.getMinV();
-                maxV = aIcon.getMaxV();
-            }
-
-            minX = aX + (float)aRenderer.renderMinX;
-            maxX = aX + (float)aRenderer.renderMaxX;
-            minY = aY + (float)aRenderer.renderMinY;
-            minZ = aZ + (float)aRenderer.renderMinZ;
-            maxZ = aZ + (float)aRenderer.renderMaxZ;
-
             lighting.setupColor(ForgeDirection.DOWN.ordinal(), 0xffffff);
-
-            if (aRenderer.enableAO) {
-                tessellator.setColorOpaque_F(aRenderer.colorRedTopLeft, aRenderer.colorGreenTopLeft, aRenderer.colorBlueTopLeft);
-                tessellator.setBrightness(aRenderer.brightnessTopLeft);
-                tessellator.addVertexWithUV(minX, minY, maxZ, maxU, maxV);
-                tessellator.setColorOpaque_F(aRenderer.colorRedBottomLeft, aRenderer.colorGreenBottomLeft, aRenderer.colorBlueBottomLeft);
-                tessellator.setBrightness(aRenderer.brightnessBottomLeft);
-                tessellator.addVertexWithUV(minX, minY, minZ, maxU, minV);
-                tessellator.setColorOpaque_F(aRenderer.colorRedBottomRight, aRenderer.colorGreenBottomRight, aRenderer.colorBlueBottomRight);
-                tessellator.setBrightness(aRenderer.brightnessBottomRight);
-                tessellator.addVertexWithUV(maxX, minY, minZ, minU, minV);
-                tessellator.setColorOpaque_F(aRenderer.colorRedTopRight, aRenderer.colorGreenTopRight, aRenderer.colorBlueTopRight);
-                tessellator.setBrightness(aRenderer.brightnessTopRight);
-            } else {
-                tessellator.addVertexWithUV(minX, minY, maxZ, maxU, maxV);
-                tessellator.addVertexWithUV(minX, minY, minZ, maxU, minV);
-                tessellator.addVertexWithUV(maxX, minY, minZ, minU, minV);
-            }
-            tessellator.addVertexWithUV(maxX, minY, maxZ, minU, maxV);
+            aRenderer.renderFaceYNeg(aBlock, aX, aY, aZ, mIconContainer.getOverlayIcon());
         }
     }
 
