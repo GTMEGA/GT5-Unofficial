ebb3e4e1be1b6d311dc6d48a1bc69cb885cbcc48
FalsePattern <me@falsepattern.com>
2026-08-09 16:23:58 +0200

    build: update gradle and buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.falsepattern.fpgradle.FPMinecraftProjectExtension.Java.JvmDowngraderShade.ProjectIsLgpl21PlusCompatible
 
 plugins {
-    id("com.falsepattern.fpgradle-mc") version "4.0.2"
+    id("com.falsepattern.fpgradle-mc") version "4.1.0"
 }
 
 group = "gtmega"
