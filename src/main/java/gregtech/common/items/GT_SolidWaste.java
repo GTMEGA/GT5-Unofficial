b170b5fee283510d0d322161675388538516dea6
matt-159 <16054364+matt-159@users.noreply.github.com>
2026-08-09 19:11:01 -0600

    add client side debug message

--- src/main/java/gregtech/common/items/GT_SolidWaste.java
+++ src/main/java/gregtech/common/items/GT_SolidWaste.java
@@ -2,6 +2,7 @@ package gregtech.common.items;
 
 import gregtech.api.items.GT_Generic_Item;
 import gregtech.common.GT_OreVeinStats;
+import gregtech.common.misc.ClientOreVeinStats;
 import lombok.val;
 
 import net.minecraft.entity.player.EntityPlayer;
@@ -21,19 +22,31 @@ public class GT_SolidWaste extends GT_Generic_Item {
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world,
                              int x, int y, int z, int side,
                              float hitX, float hitY, float hitZ) {
-        if (world.isRemote) {
-            return false;
-        }
-
-        if (player != null && player.capabilities != null && !player.capabilities.isCreativeMode) {
+        if (player == null || player.capabilities == null || !player.capabilities.isCreativeMode) {
             return false;
         }
 
         val chunkX = x >> 4;
         val chunkY = z >> 4;
+
+        if (world.isRemote) {
+            val stats = ClientOreVeinStats.getVeinStats(world, chunkX, chunkY);
+
+            val message = String.format("CLIENT - [%d, %d] -> %s (%d / %d)",
+                                        chunkX,
+                                        chunkY,
+                                        stats.oreMix(),
+                                        stats.oresCurrent(),
+                                        stats.oresPlaced());
+
+            player.addChatMessage(new ChatComponentText(message));
+
+            return false;
+        }
+
         val stats = GT_OreVeinStats.getOreVeinStatsInChunk(world, chunkX, chunkY);
 
-        val message = String.format("[%d, %d] -> %s (%d / %d)",
+        val message = String.format("SERVER - [%d, %d] -> %s (%d / %d)",
                                     chunkX,
                                     chunkY,
                                     stats.oreMix(),
