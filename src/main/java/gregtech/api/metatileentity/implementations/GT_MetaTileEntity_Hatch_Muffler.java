3ea93460506f7a31d61c185f53c5f20e50a1c692
matt-159 <16054364+matt-159@users.noreply.github.com>
2026-08-17 12:07:44 -0600

    put back air check

--- src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_Hatch_Muffler.java
+++ src/main/java/gregtech/api/metatileentity/implementations/GT_MetaTileEntity_Hatch_Muffler.java
@@ -174,7 +174,10 @@ public class GT_MetaTileEntity_Hatch_Muffler extends GT_MetaTileEntity_Hatch {
      * @return pollution success
      */
     public boolean polluteEnvironment(MetaTileEntity mte) {
-        return false;
+        var bmte = this.getBaseMetaTileEntity();
+        var frontFace = bmte.getFrontFacing();
+
+        return bmte.getAirAtSide(frontFace);
     }
 
     /**
