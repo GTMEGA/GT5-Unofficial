5a52427ad764f4ce7f2998cd4e633ae4f23909a4
Houston <houston.russ@hotmail.com>
2026-08-31 15:02:50 -0400

    lower fluid cap in the other extractor

--- src/main/java/gregtech/common/tileentities/machines/steam/GT_MetaTileEntity_Extractor_Steel.java
+++ src/main/java/gregtech/common/tileentities/machines/steam/GT_MetaTileEntity_Extractor_Steel.java
@@ -43,7 +43,7 @@ public class GT_MetaTileEntity_Extractor_Steel extends GT_MetaTileEntity_BasicMa
 
     @Override
     public int getCapacity() {
-        return 16_000;
+        return 6_000;
     }
 
     @Override
